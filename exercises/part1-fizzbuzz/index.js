29c2d9d081fc60f9171c8b73aa458d0a7ac99b17
tomsun1999 <yhssun@ucdavis.edu>
2022-10-05 01:05:14 -0400

    finish part 1-4

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -25,6 +25,27 @@ Start code
 
 ===================== */
 
+let  output  =  '' ;
+
+for (let i = 1; i < 101; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+        output += 'FizzBuzz\n';
+    }
+    else if (i % 3 == 0 ) {
+        output += 'Fizz\n';
+    }
+    else if(i % 5 == 0 ) {
+        output += 'Buzz\n';
+    }
+    else {
+        output += toString(i + '\n');
+    }
+}
+
+console.log(output);
+
+/* =====================
+End code
 
 
 
