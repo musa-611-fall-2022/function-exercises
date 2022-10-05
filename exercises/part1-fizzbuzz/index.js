b1eb0bab184759adba8d11823098fc9f4ed85155
Jonathon Sun <jonathonsun03@gmail.com>
2022-10-05 17:49:10 -0400

    Finished section 1 and 2

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -25,7 +25,53 @@ Start code
 
 ===================== */
 
+// question 1
+let i = 0
+while(i <=100 ){
+    console.log(i)
+    i++
+}
 
+
+//question 2
+
+let i = 0
+while(i <= 100) {
+    if (i/3 % 1 = 0) {
+      console.log("NOT WORKING")  
+    }
+    console.log(i)
+    i++
+}
+
+
+let i = 0
+while(i <=100 ){
+    if (i/3 % 1 === 0) {
+        console.log("fizz")
+    } else if (i/5 % 1 === 0) {
+        console.log("buzz")
+    } else {
+        console.log(i)
+    }
+    i++
+}
+
+
+// question 3
+let i = 0
+while(i <=100 ){
+    if (i/5 % 1 === 0 && i/3 % 1 ===0) {
+        console.log("fizzbuzz")
+    } else if (i/5 % 1 === 0) {
+        console.log("buzz")
+    } else if (i/3 % 1 === 0) {
+        console.log("fizz") 
+    } else {
+        console.log(i)
+    }
+    i++
+}
 
 
 /* =====================
--- exercises/part2-basic-functions/index.js
+++ exercises/part2-basic-functions/index.js
@@ -13,7 +13,10 @@ in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
 
-let age = (birthYear) => {};
+let age = (birthYear) => {
+    let result = 2022 - birthYear
+    return result
+};
 
 console.log('age success:', age(1971) === 51);
 
@@ -22,7 +25,10 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = () => {};
+let plusOne = (x) => {
+    let result = x + 1;
+    return result;
+};
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -31,7 +37,10 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = () => {};
+let timesThree = (x) => {
+    let result = x * 3;
+    return result;
+};
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -40,7 +49,10 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = () => {};
+let add = (a, b) => {
+    let result = a + b;
+    return result
+};
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -49,7 +61,10 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = () => {};
+let multiply = (a, b) => {
+    let result = a * b;
+    return result;
+};
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -58,7 +73,10 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = () => {};
+let valueAtIndex = (Index, Value) => {
+    let result = Index[Value];
+    return result
+};
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
@@ -67,7 +85,10 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = () => {};
+let valueAtKey = (Key, Value) => {
+    let result = Key[Value];
+    return result;
+};
 
 console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar');
 
@@ -75,7 +96,10 @@ console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar'
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-let isEven = () => {};
+let isEven = (i) => {
+    let result = i/2 % 1 === 0
+    return result
+};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -83,6 +107,9 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-let isOdd = () => {};
+let isOdd = (i) => {
+    let result = i % 2 === 1;
+    return result;
+};
 
 console.log('isOdd success:', isOdd(4) === false && isOdd(5) === true);
