1cd4d837acf64115b2ad5dffb634ed620a6e92af
MicahEpstein <mepstein@risd.edu>
2022-10-09 15:20:46 -0400

    part2 complete!

--- exercises/part2-basic-functions/index.js
+++ exercises/part2-basic-functions/index.js
@@ -13,12 +13,19 @@ in years. (Let's just assume this person was born January 1 at 12:01 AM)
 Example: "age(2000) should return 22"
 ===================== */
 
-function age (){
-    let ageYears;
-    let birthYear = prompt("What year were you born?", "YYYY");
+function agePrompt (){
+    var birthYear = prompt("What year were you born?", "YYYY");
+    var ageYears = 2022 - birthYear;
+    alert(`You are ${ageYears} years old, I think.`)
 }
 
-age();
+var birthYear;
+
+function age(birthYear){
+    return 2022 - birthYear;
+}
+
+/*age();*/
 
 /*let age = (birthYear) => {};
 
@@ -29,52 +36,108 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = () => {};
+function addOnePrompt (){
+    var numberString = prompt("What number would you like to add 1 to?")
+    var number = Number(numberString)
+    var newNumber = number + 1;
+    alert(`${number} + 1 = ${newNumber}`)
+}
 
-console.log('plusOne success:', plusOne(99) === 100);
+var number
+
+function plusOne (number){
+    return number + 1;
+}
+
+
+/*addOne();*/
 
 /* =====================
 Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = () => {};
+function multiplyThreePrompt (){
+    var numberString = prompt("What number would you like to multiply 3 by?")
+    var number = Number(numberString)
+    var newNumber = number * 3;
+    alert(`${number} * 3 = ${newNumber}`)
+}
 
-console.log('timesThree success:', timesThree(33) === 99);
+var number
+
+function timesThree (number){
+    return number * 3;
+}
+
+/*multiplyThree();*/
 
 /* =====================
 Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = () => {};
+function numberAdderPrompt (){
+    var firstNumberString = prompt("What is the first number you want to add?")
+    var firstNumber = Number(firstNumberString)
+    var secondNumberString = prompt(`What is the number you want to add to ${firstNumberString}?`)
+    var secondNumber = Number(secondNumberString)
+    var result = firstNumber + secondNumber;
+    alert(`${firstNumber} + ${secondNumber} = ${result}`)
+}
 
-console.log('add success:', add(4, 6) === 10);
+var firstNumber;
+var secondNumber;
+
+function add (firstNumber,secondNumber){
+    return firstNumber * secondNumber;
+}
+
+/*numberAdder();*/
 
 /* =====================
 Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = () => {};
+function numberMultiplier (){
+    var firstNumberString = prompt("What is the first number you want to multipy?")
+    var firstNumber = Number(firstNumberString)
+    var secondNumberString = prompt(`What is the number you want to multiply to ${firstNumberString}?`)
+    var secondNumber = Number(secondNumberString)
+    var result = firstNumber * secondNumber;
+    alert(`${firstNumber} * ${secondNumber} = ${result}`)
+}
 
-console.log('multiply success:', multiply(4, 6) === 24);
+function multiply (firstNumber,secondNumber){
+    return firstNumber * secondNumber;
+}
+
+
+
+/*numberMultiplier();*/
 
 /* =====================
 Instructions: "Write a function that returns the value of an array at a specified index"
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = () => {};
+var array;
+var index;
 
-console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
+function valueAtIndex(array,index){
+    return array.at(index);
+}
+
 
 /* =====================
 Instructions: "Write a function that returns the value of an object at a specified key"
 Example: "valueAtKey({'name': 'Nathan'}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = () => {};
+function valueAtKey(object,key){
+    return object[key];
+}
 
 console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar');
 
@@ -82,7 +145,13 @@ console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar'
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-let isEven = () => {};
+function isEven(number){
+    if (number % 2 == 0){
+        return true;
+    } else{
+        return false;
+    }
+}
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -90,6 +159,13 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-let isOdd = () => {};
+function isOdd(number){
+    if (number % 2 == 1){
+        return true;
+    } else{
+        return false;
+    }
+}
+
 
 console.log('isOdd success:', isOdd(4) === false && isOdd(5) === true);
