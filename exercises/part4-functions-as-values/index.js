a926c8a77e7489e5cd41f9ec31952b18d7ba060b
YuhaoChrisJ <yuhaojia98@g.ucla.edu>
2022-10-16 14:01:16 -0400

    YuhaoJia_v3

--- exercises/part4-functions-as-values/index.js
+++ exercises/part4-functions-as-values/index.js
@@ -17,13 +17,13 @@ Instructions: Write a function which takes an array and returns a new array,
   on whether the item satisfies some condition).
 ===================== */
 let filter = (arr, pred) => {
-  let filterArr=[];
+  let newArr=[];
   for (const num of arr){
     if (pred(num)){
       newArr.push(num);
     }
   }
-  return filterArr;
+  return newArr;
 };
 
 console.log('filter success #1:', _(filter([1, 2, 3, 4, 5, 4, 4], isEven)).isEqual([2, 4, 4, 4]));
