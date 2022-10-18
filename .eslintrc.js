9cd884f1178c7b851fa455eaefbd3cb4bba4b2ca
miaomiao612 <dddoctorr612@gmail.com>
2022-10-18 23:16:06 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: miaomiao612 dddoctorr612@gmail.com
  * @Date: 2022-10-06 01:58:19
  * @LastEditors: miaomiao612 dddoctorr612@gmail.com
- * @LastEditTime: 2022-10-15 05:07:13
+ * @LastEditTime: 2022-10-18 23:14:48
  * @FilePath: \function-exercises\.eslintrc.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        node: true
     },
     "extends": [
         "eslint:recommended",
@@ -26,5 +27,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-    }
-}
+    }}
+
+
