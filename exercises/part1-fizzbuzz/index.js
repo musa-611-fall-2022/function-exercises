94bba110170424dde21ba6f940a7283152df523d
MicahEpstein <mepstein@risd.edu>
2022-10-23 15:48:47 -0400

    making more specifc

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -26,10 +26,10 @@ Start code
 ===================== */
 
 for(let i = 1; i <= 100; i++){
-  if (i % 3 == 0)  {
+  if (i % 3 == 0 && i % 5 !== 0)  {
     console.log("Fizz");
   }
-  if (i % 5 == 0) {
+  if (i % 5 == 0 && i % 3 !== 0) {
     console.log("Buzz");
   }
    if (i % 3 == 0 && i % 5 == 0){
