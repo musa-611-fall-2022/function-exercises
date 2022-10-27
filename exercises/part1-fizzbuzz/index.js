72df5a7ab7b63821c5ff5a67e5d588a70bba73c1
YuewenDai <112583582+YuewenDai@users.noreply.github.com>
2022-10-27 01:23:09 -0400

    Add files via upload

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -24,7 +24,12 @@ Believe it or not, this is a common programming challenge in job interviews!
 Start code
 
 ===================== */
-
+for(let i = 0; i <=100; i++){
+if(i%15==0) console.log('FizzBuzz') ;
+else if (i%3==0) console.log('Fizz');
+else if (i%5==0) console.log('Buzz');
+else console.log(i);
+}
 
 
 
