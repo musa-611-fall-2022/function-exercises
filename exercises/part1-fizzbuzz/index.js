fd8f0e3d96ea3b20abf6ef0268c03f4e09f46cc6
sim-py <98434506+sim-py@users.noreply.github.com>
2022-11-04 11:27:23 -0400

    Check2

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -25,22 +25,22 @@ Start code
 
 ===================== */
 
-function FizzBuzz () {
-    for (let num = 1; num <= 100; num++ ) {
-        if (num % 15 === 0) {
-            console.log("FizzBuzz");
-        }
-        else if (num % 5 === 0) {
-            console.log("Buzz");
-        }
-        else if (num % 3 === 0 ) {
-            console.log("Fizz");
-        }
-        else {
-            console.log(num)
-        }
+
+for (let num = 1; num <= 100; num++ ) {
+    if (num % 15 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (num % 5 === 0) {
+        console.log("Buzz");
+    }
+    else if (num % 3 === 0 ) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(num)
     }
 }
+
 
 
 /* =====================
