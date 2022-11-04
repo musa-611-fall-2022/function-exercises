26353dbc5bce37c378658bc257a37275f87e14fa
sim-py <98434506+sim-py@users.noreply.github.com>
2022-11-04 11:21:54 -0400

    Check1

--- exercises/part3-iterators/index.js
+++ exercises/part3-iterators/index.js
@@ -16,7 +16,19 @@ Instructions: Write a function which counts the number of times a value occurs i
 Example: countItem(['a', 'b', 'a'], 'a') should return 2
 ===================== */
 
-let countItem = (arr, item) => {};
+
+let countItem = (arr, item) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] == item){
+      count = count + 1;
+    } else {
+      count;
+    }
+  }
+  return count;
+};
+
 
 console.log('countItem success:', countItem([1, 2, 3, 4, 5, 4, 4], 4) === 3);
 
@@ -27,8 +39,29 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let isEven = (n) => {};
-let filterEven = (arr) => {};
+let isEven = (num) => {
+  let div2 = num%2;
+  if (div2 == 0) {
+      return true;
+  } else {
+      return false;
+  }
+};
+
+let filterEven = (arr) => {
+  let arrEven = [];
+  let index = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (isEven(arr[i]) == true) {
+      arrEven[index] = arr[i];
+      index = index + 1;
+    } else {
+      arrEven;
+    }
+  }
+  return arrEven;
+};
+
 
 console.log('filterEven success:', _(filterEven([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 4, 4, 4]));
 
@@ -38,8 +71,28 @@ Instructions: Write a function which takes an array and returns a new array with
   function MUST use the isOdd function and MUST NOT change the original array.
 ===================== */
 
-let isOdd = (n) => {};
-let filterOdd = (arr) => {};
+let isOdd = (num) => {
+  let div2 = num%2;
+  if (div2 != 0) {
+      return true;
+  } else {
+      return false;
+  }
+};
+
+let filterOdd = (arr) => {
+  let arrOdd = [];
+  let index = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (isOdd(arr[i]) == true) {
+      arrOdd[index] = arr[i];
+      index = index + 1;
+    } else {
+      arrOdd;
+    }
+  }
+  return arrOdd;
+};
 
 console.log('filterOdd success:', _(filterOdd([1, 2, 3, 4, 5, 4, 4])).isEqual([1, 3, 5]));
 
@@ -50,8 +103,17 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let plusOne = (n) => {};
-let mapIncrement = (arr) => {};
+let plusOne = (n) => {
+  return n + 1;
+};
+
+let mapIncrement = (arr) => {
+  let add1 = [];
+  for (let i = 0; i < arr.length; i++){
+    add1[i] = plusOne(arr[i]);
+  }
+  return add1;
+};
 
 console.log('mapIncrement success:', _(mapIncrement([1, 2, 3, 4, 5, 4, 4])).isEqual([2, 3, 4, 5, 6, 5, 5]));
 
@@ -62,8 +124,16 @@ Instructions: Write a function which takes an array and returns a new array with
   original array.
 ===================== */
 
-let timesThree = (n) => {};
-let mapTriple = (arr) => {};
+let timesThree = (n) => {
+  return n*3;
+};
+let mapTriple = (arr) => {
+  let triple = [];
+  for (let i=0; i<arr.length; i++) {
+    triple[i] = timesThree(arr[i]);
+  }
+  return triple;
+};
 
 console.log('mapTriple success:', _(mapTriple([1, 2, 3, 4, 5, 4, 4])).isEqual([3, 6, 9, 12, 15, 12, 12]));
 
@@ -74,8 +144,17 @@ Instructions: Write a function which takes an array and returns the sum of all
   array.
 ===================== */
 
-let add = (n1, n2) => {};
-let reduceSum = (arr) => {};
+let add = (n1, n2) => {
+  return n1 + n2;
+};
+
+let reduceSum = (arr) => {
+  let numAdd = 0;
+  for (let i = 0; i < arr.length; i++){
+    numAdd = add(numAdd, arr[i]);
+  }
+  return numAdd;
+};
 
 console.log('reduceSum success:', reduceSum([1, 2, 3, 4, 5, 4, 4]) === 23);
 
@@ -86,7 +165,16 @@ Instructions: Write a function which takes an array and returns the product of
   original array.
 ===================== */
 
-let multiply = (n1, n2) => {};
-let reduceProduct = (arr) => {};
+let multiply = (n1, n2) => {
+  return n1*n2;
+};
+
+let reduceProduct = (arr) => {
+  let mul = 1;
+  for (let i=0; i<arr.length; i++) {
+    mul = multiply(mul, arr[i]);
+  }
+  return mul;
+};
 
 console.log('reduceProduct success:', reduceProduct([1, 2, 3, 4, 5, 4, 4]) === 1920);
