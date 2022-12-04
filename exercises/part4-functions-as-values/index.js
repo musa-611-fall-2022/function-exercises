7c8010d75c85f6157bc8756ab20f2dd743484981
xueningz22 <xueningzhang22@gmail.com>
2022-12-04 15:52:00 -0500

    function exercise part 4

--- exercises/part4-functions-as-values/index.js
+++ exercises/part4-functions-as-values/index.js
@@ -17,7 +17,17 @@ Instructions: Write a function which takes an array and returns a new array,
   on whether the item satisfies some condition).
 ===================== */
 
-let filter = (arr, pred) => {};
+let filter = (arr, pred) => {
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (pred(arr[i])) {
+          results.push(arr[i]);
+      }
+  }
+  return results;
+};
+
+console.log(_(filter([1, 2, 3, 4, 5, 4, 4], isEven)))
 
 console.log('filter success #1:', _(filter([1, 2, 3, 4, 5, 4, 4], isEven)).isEqual([2, 4, 4, 4]));
 console.log('filter success #2:', _(filter([1, 2, 3, 4, 5, 4, 4], isOdd)).isEqual([1, 3, 5]));
@@ -27,7 +37,14 @@ Instructions: Write a function which takes an array and returns a new array,
   where each item has a function applied to it.
 ===================== */
 
-let map = (arr, func) => {};
+let map = (arr, func) => {
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+      results.push(func(arr[i]));
+  }
+  return results;
+};
+
 
 console.log('map success #1:', _(map([1, 2, 3, 4, 5, 4, 4], plusOne)).isEqual([2, 3, 4, 5, 6, 5, 5]));
 console.log('map success #2:', _(map([1, 2, 3, 4, 5, 4, 4], timesThree)).isEqual([3, 6, 9, 12, 15, 12, 12]));
@@ -52,7 +69,15 @@ Instructions: Write a function which takes an array and returns the value of
 
 ===================== */
 
-let reduce = (arr, func, initial) => {};
+let reduce = (arr, func, initial) => {
+  let results = initial;
+  results = func(results, arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+      results = func(results, arr[i])
+  }
+  return results;
+};
+
 
 console.log('reduce success #1:', reduce([1, 2, 3, 4, 5, 4, 4], add, 0) === 23);
 console.log('reduce success #2:', reduce([1, 2, 3, 4, 5, 4, 4], multiply, 1) === 1920);
@@ -65,6 +90,12 @@ Bonus: Create a function called sumSquares that takes an array and returns
   `multiply` functions that you developed before).
 ===================== */
 
-let sumSquares = (arr) => {};
+let sumSquares = (arr) => {
+  let results = reduce(
+      arr, (x, y) => x + y * y, 0
+  );
+  return results;
+
+};
 
 console.log('sumSquares success:', sumSquares([1, 2, 3, 4]) === 30);
