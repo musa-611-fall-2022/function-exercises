3043f4fea23f2f4cdf1273c9035583a95552a60e
xueningz22 <xueningzhang22@gmail.com>
2022-12-04 15:08:28 -0500

    function exercise part 1

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -25,7 +25,24 @@ Start code
 
 ===================== */
 
+let result = '';
 
+for (let i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+        result += 'Fizz\n';
+    }
+    else if(i % 5 == 0 && i % 3 != 0) {
+        result += 'Buzz\n';
+    }
+    else if (i % 5 == 0 && i % 3 == 0) {
+        result += 'FizzBuzz\n';
+    }
+    else {
+        result += toString(i + '\n');
+    }
+}
+
+console.log(result);
 
 
 /* =====================
